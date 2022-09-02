cae5f3b10f2ac4907d89f3c0d02ba555b12d1ede
vibracode <codevibra@gmail.com>
2022-09-02 23:39:56 +0430

    layout done!

---
+++ components/ChatBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function ChatBox() {
+  return (
+    <Container>
+        this is chat box
+    </Container>
+  )
+}
+
+export default ChatBox
+
+const Container = styled.div`
+flex: 0.7;
+    background-color: #F0F2F5;
+`
---
+++ components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function sidebar() {
+  return (
+    <Container>sidebar</Container>
+  )
+}
+export default sidebar
+
+const Container=styled.div`
+flex: 0.3;
+background-color:white;
+
+`
