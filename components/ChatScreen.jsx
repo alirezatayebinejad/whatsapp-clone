ab20af16a75150aba6fbf908c68d1eb1825eab65
vibracode <codevibra@gmail.com>
2022-09-05 20:35:37 +0430

    chatscreen and message

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -1,14 +1,179 @@
-import React from "react";
 import styled from "styled-components";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { auth, db } from "../firebase";
+import { useRouter } from "next/router";
+import { Avatar, IconButton } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import MicIcon from "@mui/icons-material/Mic";
+import { collection, doc, orderBy, query, setDoc, Timestamp, addDoc, where } from "firebase/firestore";
+import Message from "./Message";
+import { useRef, useState } from "react";
+import getRecipientEmail from "../utils/getRecipientEmail";
+import TimeAgo from "timeago-react";
 
-function ChatScreen() {
+function Chatscreen({ chat, messages }) {
+	const [user] = useAuthState(auth);
+	const [input, setInput] = useState("");
+	const endOfMessagesRef = useRef(null);
+	const router = useRouter();
+	const [messagesSnapshot] = useCollection(query(collection(db, `chats/${router.query.id}/messages`), orderBy("timestamp", "asc")));
+	const recipientEmail = getRecipientEmail(chat.users, user);
+	const [recipientSnapshot] = useCollection(query(collection(db, "users"), where("email", "==", recipientEmail)));
+
+	const showMessages = () => {
+		if (messagesSnapshot) {
+			return messagesSnapshot.docs.map((message) => (
+				<Message
+					key={message.id}
+					user={message.data().user}
+					message={{
+						...message.data(),
+						timestamp: message.data().timestamp?.toDate().getTime(),
+					}}
+				/>
+			));
+		} else {
+			return JSON.parse(messages).map((message) => <Message key={message.id} user={message.user} message={message} />);
+		}
+	};
+
+	const scrollToBottom = () => {
+		endOfMessagesRef.current.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+		});
+	};
+
+	const sendMessage = (e) => {
+		e.preventDefault();
+
+		const docRef = doc(db, `chats/${user.uid}`);
+		setDoc(
+			docRef,
+			{
+				lastSeen: Timestamp.now(),
+			},
+			{ merge: true }
+		);
+
+		const colRef = collection(db, `chats/${router.query.id}/messages`);
+		addDoc(colRef, {
+			timestamp: Timestamp.now(),
+			message: input,
+			user: user.email,
+			photoURL: user.photoURL,
+		});
+
+		setInput("");
+		scrollToBottom();
+	};
+
+	const recipient = recipientSnapshot?.docs?.[0]?.data();
+
 	return (
 		<Container>
-			<h1>chatScreen</h1>
+			<Header>
+				<Avatar />
+
+				<HeaderInformation>
+					<h3>{recipientEmail}</h3>
+					{recipientSnapshot ? (
+						<p>Last active: {recipient?.lastSeen?.toDate() ? <TimeAgo datetime={recipient?.lastSeen?.toDate()} /> : "Unavailable"}</p>
+					) : (
+						<p>Loading last active...</p>
+					)}
+				</HeaderInformation>
+				<HeaderIcons>
+					<IconButton>
+						<AttachFileIcon />
+					</IconButton>
+					<IconButton>
+						<MoreVertIcon />
+					</IconButton>
+				</HeaderIcons>
+			</Header>
+
+			<MessageContainer>
+				{showMessages()}
+				<EndOfMessage ref={endOfMessagesRef} />
+			</MessageContainer>
+
+			<InputContainer>
+				<IconButton>
+					<InsertEmoticonIcon />
+				</IconButton>
+				<Input value={input} onChange={(e) => setInput(e.target.value)} />
+				<button hidden disabled={!input} type="submit" onClick={sendMessage}>
+					Send Message
+				</button>
+				<IconButton>
+					<MicIcon />
+				</IconButton>
+			</InputContainer>
 		</Container>
 	);
 }
 
-export default ChatScreen;
+export default Chatscreen;
 
 const Container = styled.div``;
+
+const Header = styled.div`
+	position: sticky;
+	background-color: white;
+	z-index: 100;
+	top: 0;
+	display: flex;
+	padding: 11px;
+	height: 80px;
+	align-items: center;
+	border-bottom: 1px solid whitesmoke;
+`;
+
+const HeaderInformation = styled.div`
+	margin-left: 15px;
+	flex: 1;
+	> h3 {
+		margin-bottom: 3px;
+	}
+	> p {
+		font-size: 14px;
+		color: gray;
+	}
+`;
+
+const HeaderIcons = styled.div``;
+
+const MessageContainer = styled.div`
+	padding: 30px;
+	background-color: #e5ded8;
+	min-height: 90vh;
+`;
+
+const EndOfMessage = styled.div`
+	margin-bottom: 50px;
+`;
+
+const InputContainer = styled.form`
+	display: flex;
+	align-items: center;
+	padding: 10px;
+	position: sticky;
+	bottom: 0;
+	background-color: white;
+	z-index: 100;
+`;
+
+const Input = styled.input`
+	flex: 1;
+	outline: 0;
+	border: none;
+	border-radius: 10px;
+	background-color: whitesmoke;
+	padding: 20px;
+	margin-left: 15px;
+	margin-right: 15px;
+`;
